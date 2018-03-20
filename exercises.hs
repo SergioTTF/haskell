098936e03360caf00d2c53dd9d6b6c0579eb40e7
Sergio Torres Teixeira Filho <sttf@WINDOWS.CIN.UFPE.BR>
2018-03-20 11:45:17 -0300

    List Exercises

--- exercises.hs
+++ exercises.hs
@@ -15,3 +15,49 @@ digitsList (a:z)
 sumPairsList :: [(Int, Int)] -> [Int]
 sumPairsList [] = []
 sumPairsList (a:z) = (fst a + snd a) : sumPairsList z
+
+doubleList :: [Int] -> [Int]
+doubleList [] = []
+doubleList (a:z) = (2*a) : doubleList z
+
+type Person = String
+type Book = String
+type DataBase = [(Person, Book)]
+
+books :: DataBase -> Person -> [Book]
+books [] _ = []
+books (a:z) p
+        | fst a == p = snd a : books z p
+        | otherwise = books z p
+
+rents :: DataBase -> Book -> [Person]
+rents [] _ = []
+rents (a:z) l
+        | snd a == l = fst a : rents z l
+        | otherwise = rents z l
+
+rented :: DataBase -> Book -> Bool
+rented [] _ = False
+rented (a:z) l
+        | snd a == l = True
+        | otherwise = rented z l
+
+rentsNumber :: DataBase -> Person -> Int
+rentsNumber [] _ = 0
+rentsNumber (a:z) p
+        | fst a == p = 1 + rentsNumber z p 
+        | otherwise = rentsNumber z p
+
+rent :: DataBase -> Person -> Book -> DataBase
+rent [] p l = [(p, l)]
+rent d p l = (p, l) : d
+
+returnBook :: DataBase -> Person -> Book -> DataBase
+returnBook [] _ _ = []
+returnBook (a:z) p l
+        | fst a == p && snd a == l = z
+        | otherwise = a : returnBook z p l
+
+
+--*member :: [Int] -> Int -> Bool
+--member []
