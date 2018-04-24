3f8b37197d9794b6cd1b8f15f3c17d9dfce741ad
Sergio Torres Teixeira Filho <sttf@WINDOWS.CIN.UFPE.BR>
2018-04-24 11:43:32 -0300

    Continue with list 2

--- list2.hs
+++ list2.hs
@@ -12,3 +12,14 @@ isPrime a = a == last (eratosthenes [2..a])
     eratosthenes :: [Int] -> [Int]
     eratosthenes [] = []
     eratosthenes (i:j) = i : (eratosthenes (filter (\x -> mod x i /= 0) j))
+
+type Point = (Double, Double, Double)
+
+distance :: Point -> Point -> Double
+distance (xA, yA, zA) (xB, yB, zB) = sqrt ((xA - xB)**2 + (yA - yB)**2 + (zA - zB)**2)
+
+powSum :: Int -> [Int]
+powSum a = [x * x | x <- [1..a]]
+
+--grid :: Int -> Int -> [(Int, Int)]
+-- grid m n = [(x, y) | x <-[0..m]]
