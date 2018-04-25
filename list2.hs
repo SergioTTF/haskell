18b82f0992e1a3d370297119b5890362b5023f2c
Sergio Torres <sttf@cin.ufpe.br>
2018-04-25 02:38:11 -0300

    List 2 Almost there

--- list2.hs
+++ list2.hs
@@ -21,5 +21,43 @@ distance (xA, yA, zA) (xB, yB, zB) = sqrt ((xA - xB)**2 + (yA - yB)**2 + (zA - z
 powSum :: Int -> [Int]
 powSum a = [x * x | x <- [1..a]]
 
---grid :: Int -> Int -> [(Int, Int)]
--- grid m n = [(x, y) | x <-[0..m]]
+grid :: Int -> Int -> [(Int, Int)]
+grid m n = [(x, y) | x <-[0..m], y <- [0..n]]
+
+square :: Int -> [(Int, Int)]
+square n = [x | x <- grid n n, fst x /= snd x]
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge a [] = a
+merge [] b = b
+merge (a:z) (x:xs)
+    | a < x = a : merge z (x:xs)
+    | otherwise = x : merge (a:z) xs
+
+halve :: [a] -> ([a],[a])
+halve a = splitAt (length a `div` 2) a
+
+msort :: Ord a => [a] -> [a]
+msort [] = []
+msort [x] = [x]
+msort a = merge (msort (fst (halve a))) (msort (snd (halve a)))
+
+applyFunc :: [Int->Int] -> [Int] -> [[Int]]
+applyFunc [] _ = []
+applyFunc (f:fs) l = [f x | x <- l ] : applyFunc fs l
+
+data WeekDay = Sunday | Monday | Tuesday | Wednesday | Thursday | Friday | Saturday deriving (Eq, Ord, Show, Enum)
+
+orderDay :: [WeekDay] -> [WeekDay]
+orderDay [] = []
+orderDay [a] = [a]
+orderDay (x:xs) = orderDay [y | y<-xs, y <= x] ++(x : orderDay [y | y<-xs, y > x])
+
+sameDate :: [(WeekDay, Int)] -> WeekDay -> [Int]
+sameDate [] _ = []
+sameDate (x:xs) d
+        | fst x == d = snd x : sameDate xs d
+        | otherwise = sameDate xs d
+
+printMonth :: WeekDay -> [(Int, WeekDay)]
+printMonth d = [(x, y) | x <- [1..30], y <- [d..]]
